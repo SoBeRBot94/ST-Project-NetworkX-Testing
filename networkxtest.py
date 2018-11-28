976358faa18b7bf3a0817d214372ee0725f52e60
vahe2002 <florencia73000@gmail.com>
2018-11-28 20:42:26 +0300

    skeleton file to the master branch

---
+++ networkxtest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+'''
+Softwaretesting project: Networkx 1.11
+
+Team Number: 2
+Student Names: Sudarsan Bhargavan, Valeria Helle, Agnes Lind, Anna Westergren
+'''
+
+
+import unittest
+import networkx as nx
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
