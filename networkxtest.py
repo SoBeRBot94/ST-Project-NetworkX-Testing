186ee26b9ba78d603c7be84909d74f90d1fdacec
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2019-01-08 00:52:16 +0100

    Test Case No 1a: List of Nodes

--- networkxtest.py
+++ networkxtest.py
@@ -11,8 +11,13 @@ Student Names: Sudarsan Bhargavan, Valeria Helle, Agnes Lind, Anna Westergren
 import unittest
 import networkx as nx
 
-
-
+class NodesTest(unittest.TestCase):
+    def test_integer_nodes(self):
+        '''Should return the copy of the Graph's nodes in a list - Integer'''
+        graph = nx.Graph([(1,2),(1,4),(1,5),(2,3),(3,4),(3,5),(5,6),(6,7)])
+        nodeList = list(nx.nodes(graph))
+        result = [1,2,3,4,5,6,7]
+        self.assertListEqual(nodeList, result)
 
 
 if __name__ == '__main__':
