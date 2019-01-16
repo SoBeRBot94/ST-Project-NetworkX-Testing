8e682c3780f5f46d92d7617caa30391d4b974e31
Agnes Lind <agnes.lind@hotmail.com>
2019-01-16 14:39:53 +0100

    Test Case No 1a: Is Weighted

--- networkxtest.py
+++ networkxtest.py
@@ -11,8 +11,14 @@ Student Names: Sudarsan Bhargavan, Valeria Helle, Agnes Lind, Anna Westergren
 import unittest
 import networkx as nx
 
+class TestAttributes(unittest.TestCase):
+	def test_is_weighted(self):
+		graph=nx.Graph()
+		graph.add_nodes_from([1,2,3,4])
+		graph.add_edges_from([(1,2),(1,3),(1,4)])
 
-
+		output=nx.is_weighted(graph)
+		self.assertFalse(output)
 
 
 if __name__ == '__main__':
