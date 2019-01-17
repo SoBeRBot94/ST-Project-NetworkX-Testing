d5d401b2475d38bc49fb63606dfc3ad17520147a
Agnes Lind <agnes.lind@hotmail.com>
2019-01-17 16:54:54 +0100

    Added Test Cases 3a, 3b, 3c

--- networkxtest.py
+++ networkxtest.py
@@ -12,42 +12,91 @@ import unittest
 import networkx as nx
 
 class TestAttributes(unittest.TestCase):
-	#Test Case No 1a: Is Weighted with no weight
-	def test_is_weighted(self):
+
+	
+	def test_is_weighted_without_weights(self):
+		'''Test Case No 1a: Is Weighted with no weight'''
 		graph=nx.Graph()
 		graph.add_nodes_from([1,2,3,4])
 		graph.add_edges_from([(1,2),(1,3),(1,4)])
 		output=nx.is_weighted(graph)
 		self.assertFalse(output)
 
-	#Test Case No 1b: Is Weighted with positive weight
+	def test_is_weighted_positive_weights(self):
+		'''Test Case No 1b: Is Weighted with positive weight'''
+		graph=nx.Graph()
+		graph.add_nodes_from([1,2,3,4])
 		graph.add_edges_from([(1, 2, {'weight' : 2}), (1, 3, {'weight' : 4}), (1, 4, {'weight' : 6})])
 		output=nx.is_weighted(graph)
 		self.assertTrue(output)
 
-	#Test Case No 1c: Is Weighted with negative weight
+	def test_is_weighted_negative_weights(self):
+		'''Test Case No 1c: Is Weighted with negative weight'''
+		graph=nx.Graph()
+		graph.add_nodes_from([1,2,3,4])
 		graph.add_edges_from([(1, 2, {'weight' : -2}), (1, 3, {'weight' : -4}), (1, 4, {'weight' : -6})])
 		output=nx.is_weighted(graph)
 		self.assertTrue(output)
 
 
-	#Test Case No 2a: Is Negatively Weighted with no weight
-	def test_is_negatively_weighted(self):
+	def test_is_negatively_weighted_without_weights(self):
+		'''Test Case No 2a: Is Negatively Weighted with no weight'''
 		graph=nx.Graph()
 		graph.add_nodes_from([5,6,7,8])
 		graph.add_edges_from([(5,6),(6,7),(7,8)])
 		output=nx.is_negatively_weighted(graph)
 		self.assertFalse(output)
 
-	#Test Case No 2b: Is Negatively Weighted with negative weight
+	def test_is_negatively_weighted_negative_weights(self):
+		'''Test Case No 2b: Is Negatively Weighted with negative weight'''
+		graph=nx.Graph()
+		graph.add_nodes_from([5,6,7,8])
 		graph.add_edges_from([(5, 6, {'weight' : -1}), (6, 7, {'weight' : -2}), (7, 8, {'weight' : -3})])
 		output=nx.is_negatively_weighted(graph)
 		self.assertTrue(output)
 
-	#Test Case No 2c: Is Negatively Weighted with positive weight
+	def test_is_negatively_weighted_positive_weights(self):
+		'''Test Case No 2c: Is Negatively Weighted with positive weight'''
+		graph=nx.Graph()
+		graph.add_nodes_from([5,6,7,8])
 		graph.add_edges_from([(5, 6, {'weight' : 1}), (6, 7, {'weight' : 2}), (7, 8, {'weight' : 3})])
 		output=nx.is_negatively_weighted(graph)
 		self.assertFalse(output)
+
+	def test_set_node_attributes_true_value(self):
+		'''Test Case No 3a: Set Node Attributes with True Value'''
+		graph=nx.Graph()
+		graph.add_nodes_from([1,2,3,4])
+		graph.add_edges_from([(1,2),(1,3),(1,4)])
+
+		nx.set_node_attributes(graph, {1:2}, name='value')
+		output = graph.node[1]['value']
+		result = 2
+		self.assertEqual(output, result)
+
+
+	def test_set_node_attributes_false_value(self):
+		'''Test Case No 3b: Set Node Attributes with False Value'''
+		graph=nx.Graph()
+		graph.add_nodes_from([1,2,3,4])
+		graph.add_edges_from([(1,2),(1,3),(1,4)])
+
+		nx.set_node_attributes(graph, {1:2}, name='value')
+		output = graph.node[1]['value']
+		result = 4
+		self.assertNotEqual(output, result)
+
+
+	def test_set_node_attributes_string(self):
+		'''Test Case No 3c: Set Node Attributes with strings'''
+		graph=nx.Graph()
+		graph.add_nodes_from([1,2,3,4])
+		graph.add_edges_from([(1,2),(1,3),(1,4)])
+
+		nx.set_node_attributes(graph, {1:'agnes'}, name='name')
+		self.assertTrue((graph.node[1]['name'] == 'agnes'))
+
+
 
 
 
