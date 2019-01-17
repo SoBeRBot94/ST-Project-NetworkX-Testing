88fff366f138464c3fb82a386d5967b844434bb6
annawestn <annawestergrenkempe@gmail.com>
2019-01-17 16:25:04 +0100

    Test Case No 1a: Freezing a Graph Structure

--- networkxtest.py
+++ networkxtest.py
@@ -11,9 +11,18 @@ Student Names: Sudarsan Bhargavan, Valeria Helle, Agnes Lind, Anna Westergren
 import unittest
 import networkx as nx
 
-
-
-
+class TestFreezingGraphStructure(unittest.TestCase):
+    def test_freeze(self):
+        '''Should return the same list of nodes after the graph is frozen'''
+        graph = nx.Graph([(1,2), (1,3), (1,4)])
+        frozenGraph = nx.freeze(graph)
+        try:
+            frozenGraph.add_edges_from([(2,5),(3,6)])
+        except nx.NetworkXError:
+            print("The Graph is Frozen !")
+        Output = list(nx.edges(frozenGraph))
+        Result = [(1,2), (1,3), (1,4)]
+        self.assertListEqual(Output, Result)
 
 if __name__ == '__main__':
     unittest.main()
