8e65553b6dc8bd65b43b28fdd17950c79d410c91
annawestn <annawestergrenkempe@gmail.com>
2019-01-23 16:23:45 +0100

    updated testcase Iaupdated testcase Ia

--- networkxtest.py
+++ networkxtest.py
@@ -33,5 +33,15 @@ class TestFreezingGraphStructure(unittest.TestCase):
         resList = [('a','b'), ('a','c'), ('a','d')]
         self.assertListEqual(output, resList)
 
+        graph = nx.Graph([('-1','-2'), ('-1','-3'), ('-1','-4')])
+        frozenGraph = nx.freeze(graph)
+        try:
+            frozenGraph.add_edges_from([('-2','-5'), ('-3','-6')])
+        except nx.NetworkXError:
+           print ("The Graph is Frozen")
+        output = sorted(list(nx.edges(frozenGraph)))
+        resList = [('-1','-2'), ('-1','-3'), ('-4','-1')]
+        self.assertListEqual(output, resList)
+
 if __name__ == '__main__':
     unittest.main()
