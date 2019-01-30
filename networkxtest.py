80548dfff59fbf1f651d0777440eca4979880139
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2019-01-30 16:53:15 +0100

    Updated TestCases

--- networkxtest.py
+++ networkxtest.py
@@ -18,30 +18,27 @@ class TestFreezingGraphStructure(unittest.TestCase):
         try:
             frozenGraph.add_edges_from([(2,5),(3,6)])
         except nx.NetworkXError:
-            print("The graph is frozen")
-        output = list(nx.edges(frozenGraph))
-        resList = [(1,2), (1,3), (1,4)]
-        self.assertListEqual(output, resList)
+            output = list(nx.edges(frozenGraph))
+            resList = [(1,2), (1,3), (1,4)]
+            self.assertListEqual(output, resList)
 
         graph = nx.Graph([('a','b'), ('a','c'), ('a','d')])
         frozenGraph = nx.freeze(graph)
         try:
             frozenGraph.add_edges_from([('e','f'), ('g','h')])
         except nx.NetworkXError:
-           print ("The Graph is Frozen")
-        output = sorted(list(nx.edges(frozenGraph)))
-        resList = [('a','b'), ('a','c'), ('a','d')]
-        self.assertListEqual(output, resList)
+            output = sorted(list(nx.edges(frozenGraph)))
+            resList = [('a','b'), ('a','c'), ('a','d')]
+            self.assertListEqual(output, resList)
 
         graph = nx.Graph([('-1','-2'), ('-1','-3'), ('-1','-4')])
         frozenGraph = nx.freeze(graph)
         try:
             frozenGraph.add_edges_from([('-2','-5'), ('-3','-6')])
         except nx.NetworkXError:
-           print ("The Graph is Frozen")
-        output = sorted(list(nx.edges(frozenGraph)))
-        resList = [('-1','-2'), ('-1','-3'), ('-4','-1')]
-        self.assertListEqual(output, resList)
+            output = sorted(list(nx.edges(frozenGraph)))
+            resList = [('-1','-2'), ('-1','-3'), ('-4','-1')]
+            self.assertListEqual(output, resList)
 
 if __name__ == '__main__':
     unittest.main()
