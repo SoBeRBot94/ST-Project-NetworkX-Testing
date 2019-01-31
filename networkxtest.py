27d0e8b531102ee26bfecfe6627680c26d1fad78
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2019-02-01 00:30:31 +0100

    Updated Density Values To Fix Platform Errors

--- networkxtest.py
+++ networkxtest.py
@@ -406,15 +406,15 @@ class GraphTest(unittest.TestCase):
 		testgraph = nx.Graph([(1,2),(1,5),(2,3),(2,5),(3,4),(4,5),(4,6)])
 		edges = len(nx.edges(testgraph))
 		nodes = len(nx.nodes(testgraph))
-		density = (2 * edges)/(nodes* (nodes-1))
-		self.assertEqual(round(density,1), round(nx.density(testgraph),1))
+		#density = (2 * edges)/(nodes* (nodes-1))
+		self.assertEqual(0.5, round(nx.density(testgraph),1))
 		#test that density calculated by the function nx.density equals to 
 		#density calculated with the density formula for directed graph
 		testgraph_di = nx.DiGraph([(1,2),(1,5),(2,3),(2,5),(3,4),(4,5),(4,6)])
 		edges_di = len(nx.edges(testgraph_di))
 		nodes_di = len(nx.nodes(testgraph_di))
-		density_di = (edges_di)/(nodes_di* (nodes_di-1))
-		self.assertEqual(round(density_di,1), round(nx.density(testgraph_di),1))
+		#density_di = (edges_di)/(nodes_di* (nodes_di-1))
+		self.assertEqual(0.2, round(nx.density(testgraph_di),1))
 		self.assertFalse(nx.density(testgraph_di) == nx.density(testgraph))
 		#test that density of an empty grpah equals to 0
 		testgraph = nx.Graph([])
