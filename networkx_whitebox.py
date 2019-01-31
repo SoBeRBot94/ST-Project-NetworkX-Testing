2627895230d202f4bfe81765ee5179e3b3c96862
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2019-01-31 19:50:17 +0100

    Test Case No 5,6,7,8 - All Cases Similar To Test Cases 1,2,3,4 Execpt Without a Key

--- networkx_whitebox.py
+++ networkx_whitebox.py
@@ -40,6 +40,33 @@ class AddEdges(unittest.TestCase):
         result = [(1,2),(1,3),(2,4),(3,4),(4,5)]
         self.assertListEqual(expOutput, result)
   
+    def test_add_edges_from_single_edge_without_key_new_vertex(self):
+        graph = nx.Graph([(1,2),(1,3),(2,4),(3,4),(4,5)])
+        graph.add_edges_from([(5,6)])
+        expOutput = list(nx.edges(graph))
+        result = [(1,2),(1,3),(2,4),(3,4),(4,5),(5,6)]
+        self.assertEqual(set(expOutput), set(result))
+
+    def test_add_edges_from_single_edge_without_key_new_node(self):
+        graph = nx.Graph([(1,2),(1,3),(2,4),(3,4),(4,5)])
+        graph.add_edges_from([(9,5)])
+        expOutput = list(nx.edges(graph))
+        result = [(1,2),(1,3),(2,4),(3,4),(4,5),(5,9)]
+        self.assertEqual(set(expOutput), set(result))
+
+    def test_add_edges_from_single_edge_without_key_new_node_new_vertex(self):
+        graph = nx.Graph([(1,2),(1,3),(2,4),(3,4),(4,5)])
+        graph.add_edges_from([(6,7)])
+        expOutput = list(nx.edges(graph))
+        result = [(1,2),(1,3),(2,4),(3,4),(4,5),(6,7)]
+        self.assertEqual(set(expOutput), set(result))
+  
+    def test_add_edges_from_single_edge_without_key_no_new_edge(self):
+        graph = nx.Graph([(1,2),(1,3),(2,4),(3,4),(4,5)])
+        graph.add_edges_from([(1,3)])
+        expOutput = list(nx.edges(graph))
+        result = [(1,2),(1,3),(2,4),(3,4),(4,5)]
+        self.assertListEqual(expOutput, result)
   
 
   
